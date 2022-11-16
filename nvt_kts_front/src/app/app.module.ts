be23285bdaffc1fe4cd05bb50ad8ad772a69be09
SimicAleksa <aleksadsimic@gmail.com>
2022-11-16 16:54:07 +0100

    Front initialization

---
+++ nvt_kts_front/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { Route, RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  {path:'',component:AppComponent}]
+  
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
