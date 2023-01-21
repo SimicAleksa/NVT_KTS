aafc9e81e0c6b315c0ad287bda1f686137fcb00b
SimicAleksa <aleksadsimic@gmail.com>
2023-01-21 15:35:14 +0100

    Pregled vozila na mapi DONE

--- nvt_kts_front/src/modules/maps/components/vehicles-on-map/vehicles-on-map.component.ts
+++ nvt_kts_front/src/modules/maps/components/vehicles-on-map/vehicles-on-map.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 // import * as L from "leaflet";
-import { latLng, tileLayer, marker, geoJSON, LayerGroup, icon } from 'leaflet';
+import { latLng, tileLayer, marker, geoJSON, LayerGroup, icon, gridLayer } from 'leaflet';
 import { MapService } from '../../services/map.service';
 import {IDriverVehicleDTO} from "../active-vehicle/driverWithVehicleDTO";
 import { Driver } from '../active-vehicle/Driver';
@@ -14,9 +14,6 @@ import * as SockJS from 'sockjs-client';
   styleUrls: ['./vehicles-on-map.component.css']
 })
 export class VehiclesOnMapComponent implements OnInit {
-  // private map!: L.Map;
-  // private centroid: L.LatLngExpression = [44.0165, 21.0059];
-
   options = {
     layers: [
       tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -28,17 +25,19 @@ export class VehiclesOnMapComponent implements OnInit {
     center: latLng(45.253434, 19.831323),
   };
 
-  drivers: any = {};
+  drivers:any= {};
   rides: any = {};
   mainGroup: LayerGroup[] = [];
   private stompClient: any;
   constructor(private mapService: MapService) { }
+  // private geoLayerRouteGroup: LayerGroup = new LayerGroup();
+  private geoLayerRouteGroupDRIVERSTHATDONTDRIVE: LayerGroup = new LayerGroup();
 
   ngOnInit(): void {
     this.mapService.getAllActiveDrivers().subscribe((ret)=>{
       for(let driver of ret){
         console.log(driver)
-          let geoLayerRouteGroup: LayerGroup = new LayerGroup();
+          // let geoLayerRouteGroup: LayerGroup = new LayerGroup();
           let markerLayer = marker([driver.currentCoords.latitude,driver.currentCoords.longitude], {
             icon: icon({
               iconUrl: './assets/images/carOnMap.png',
@@ -46,7 +45,7 @@ export class VehiclesOnMapComponent implements OnInit {
               iconAnchor: [18, 45],
           }),
         });
-        markerLayer.addTo(geoLayerRouteGroup);
+        markerLayer.addTo(this.geoLayerRouteGroupDRIVERSTHATDONTDRIVE);
         markerLayer.bindPopup(
           '<div class="card text-white bg-success mb-0" style="max-width: 13rem;">\n' +
                   '  <div class="card-body">\n' +
@@ -57,7 +56,7 @@ export class VehiclesOnMapComponent implements OnInit {
                   '  </div>\n' +
                   '</div>')
         this.drivers[driver.id] = markerLayer;
-        this.mainGroup = [...this.mainGroup, geoLayerRouteGroup];
+        this.mainGroup = [...this.mainGroup, this.geoLayerRouteGroupDRIVERSTHATDONTDRIVE];
       }
     })
 
@@ -81,16 +80,13 @@ export class VehiclesOnMapComponent implements OnInit {
               iconAnchor: [18, 45],
           }),
         });
-        // markerLayer.addTo(geoLayerRouteGroup);
+        markerLayer.addTo(geoLayerRouteGroup);
         this.drivers[driver.id] = markerLayer;
         this.mainGroup = [...this.mainGroup, geoLayerRouteGroup];
         })
       }
     });
   }
-
-  // ngOnChanges(): void{
-  // }
 
   initializeWebSocketConnection() {
     let ws = new SockJS('http://localhost:8000/socket');
@@ -103,32 +99,12 @@ export class VehiclesOnMapComponent implements OnInit {
   }
 
   openGlobalSocket() {
+    
     this.stompClient.subscribe('/map-updates/update-vehicle-position', (message: { body: string }) => {
-      let geoLayerRouteGroup: LayerGroup = new LayerGroup();
       let driver: Driver = JSON.parse(message.body);
       let existingDriver = this.drivers[driver.id];
       existingDriver.setLatLng([driver.currentCoords.latitude, driver.currentCoords.longitude]);
       existingDriver.update();
-
-      let markerLayer = marker([driver.currentCoords.latitude, driver.currentCoords.longitude], {
-        icon: icon({
-          iconUrl: './assets/images/carOnMap.png',
-          iconSize: [35, 45],
-          iconAnchor: [18, 45],
-        }),
-      });
-      markerLayer.addTo(geoLayerRouteGroup);
-      // markerLayer.bindPopup(
-      //   '<div class="card text-white bg-success mb-0" style="max-width: 13rem;">\n' +
-      //           '  <div class="card-body">\n' +
-      //           '    <h5 class="card-title">'+driver.id+'</h5>\n' +
-      //           '  </div>\n' +
-      //           '  <div class="card-body">\n' +
-      //           '    <a href="#" class="card-link text-white fs-5">Schedule</a>\n' +
-      //           '  </div>\n' +
-      //           '</div>')
-      this.drivers[driver.id] = markerLayer;
-      this.mainGroup = [...this.mainGroup, geoLayerRouteGroup];
     });
     this.stompClient.subscribe('/map-updates/new-ride', (message: { body: string }) => {
       let geoLayerRouteGroup: LayerGroup = new LayerGroup();
@@ -148,12 +124,14 @@ export class VehiclesOnMapComponent implements OnInit {
       this.mainGroup = this.mainGroup.filter((lg: LayerGroup) => lg !== this.rides[ride.id]);
       // delete this.drivers[ride.driver.id];
       // delete this.rides[ride.id];
+      window.location.reload();
     });
 
 
     this.stompClient.subscribe('/map-updates/change-RIDE-to-in-PROGRESS', (message: { body: string }) => {
       let ride: Ride = JSON.parse(message.body);
       this.mainGroup = this.mainGroup.filter((lg: LayerGroup) => lg !== this.rides[ride.id]);
+      window.location.reload();
       // delete this.drivers[ride.driver.id];
       // delete this.rides[ride.id];
     });
