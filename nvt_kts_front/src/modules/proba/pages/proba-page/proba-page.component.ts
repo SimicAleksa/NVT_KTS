1e9889323a88e90363834b4686f77d5f24386474
unknown <nevenaradesic@gmail.com>
2023-01-24 19:32:51 +0100

    dodat options meni za odabir uvezanih putnika

--- nvt_kts_front/src/modules/proba/pages/proba-page/proba-page.component.ts
+++ nvt_kts_front/src/modules/proba/pages/proba-page/proba-page.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {render} from "creditcardpayments/creditCardPayments";
 
 
 @Component({
@@ -8,36 +7,20 @@ import {render} from "creditcardpayments/creditCardPayments";
   styleUrls: ['./proba-page.component.css']
 })
 export class ProbaPageComponent implements OnInit {
-  value:string = "20";
-  
 
-  constructor(){
-    
-  }
-
-
+  fruits = [ 'Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+  selectedFruits :string[]=[];
+  constructor(){}
   ngOnInit(): void {
   }
 
-  setValue()
-  {
-    
-    //this.value = document.querySelector("#moneyValueInput")!.innerHTML;
-
-    const input = document.getElementById('moneyValueInput') as HTMLInputElement | null;
-    this.value = input!.value;    
-    console.log(this.value + "je unesena vrijednos");
-    render({
-      id: "#myPaypalButtons",
-      currency: "USD",
-      value: this.value,
-      onApprove: (details) => {
-        alert("Transaction successful");
-      }
-    })
-  }
-
-  
+  saveSelectedFruit(e:any) {
+    let fruitFromPage=e.target.value;
+    this.selectedFruits.push(String(fruitFromPage));
+    // this.fruits = this.fruits.filter(x => x === fruitFromPage);
+    console.log(this.selectedFruits);
+    console.log(this.fruits);
+ }
 
 
 }
