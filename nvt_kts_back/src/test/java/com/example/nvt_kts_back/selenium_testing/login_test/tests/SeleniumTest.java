69cc0edc36dce44d47e52005454b38c7a978e55e
SimicAleksa <aleksadsimic@gmail.com>
2023-02-03 15:20:12 +0100

    Merge

--- nvt_kts_back/src/test/java/com/example/nvt_kts_back/selenium_testing/login_test/tests/SeleniumTest.java
+++ nvt_kts_back/src/test/java/com/example/nvt_kts_back/selenium_testing/login_test/tests/SeleniumTest.java
@@ -1,22 +1,22 @@
 package com.example.nvt_kts_back.selenium_testing.login_test.tests;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 
 
 public class SeleniumTest {
     public static WebDriver webDriver;
 
-    @BeforeClass
+    @BeforeSuite
     public static void initializeWebDriver() {
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         webDriver = new ChromeDriver();
         webDriver.manage().window().maximize();
     }
 
-    @AfterClass
+    @AfterSuite
     public static void quitDriver() {
         webDriver.quit();
     }
